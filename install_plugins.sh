c645fb0c3ea47130dc41274faab27d80be2939a1
Kevin Royer <kevinroyer@brain.im>
2018-01-16 10:17:16 -0800

    Add plugins install script

---
+++ install_plugins.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+
+git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
+
+git submodule add https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
